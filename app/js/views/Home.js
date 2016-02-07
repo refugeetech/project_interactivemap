8ebd903318b921b518900ebc705c62ccdaf7c455
Olle Werme <olle.werme@gmail.com>
2016-02-07 11:04:37 +0100

    The ring

--- app/js/views/Home.js
+++ app/js/views/Home.js
@@ -4,19 +4,37 @@ import { Link } from 'react-router'
 const Home = ({
   sections
 }) =>
-  <div style={{padding: '0 1rem'}}>
+  <div style={{padding: '0 1rem', textAlign: 'center'}}>
     <h1>Welcome to Sweden</h1>
 
-    <ul>
+    <ul style={{
+      display: 'flex',
+      flexWrap: 'wrap',
+      textAlign: 'center',
+      justifyContent: 'center'}}
+    >
       {Object.values(sections).map((section, i) =>
-        <li key={i}>
-          <Link to={String(section.id)}>
-            {section.name}
-          </Link>
+        <li key={i} style={{margin: '0 1rem 2rem'}}>
+          <SectionLink {...section} />
         </li>
       )}
     </ul>
 
   </div>
+
+const Circle = ({ radius }) =>
+  <div style={{
+    width: radius,
+    height: radius,
+    border: '3px solid #eee',
+    margin: '0 auto 0.6rem',
+    borderRadius: '100%'
+  }} />
+
+const SectionLink = ({ id, name }) =>
+  <Link to={String(id)}>
+    <Circle radius="7rem" />
+    <div>{name}</div>
+  </Link>
 
 export default Home
