ab5e6d830ffd4e039d93051e7af57d732734d3d3
IsabelldVM <Isabelldvm@gmail.com>
2016-02-07 10:38:39 +0100

    Remove dummy data

--- app/js/views/Home.js
+++ app/js/views/Home.js
@@ -1,26 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-const menuItems = [{
-  path: '/work',
-  label: 'Work'
-},{
-  path: '/health',
-  label: 'Health'
-},{
-  path: '/education',
-  label: 'Education'
-},{
-  path: '/health',
-  label: 'Health'
-},{
-  path: '/support',
-  label: 'Support'
-},{
-  path: '/stuff',
-  label: 'Stuff'
-}]
-
 const Home = ({
   sections
 }) =>
