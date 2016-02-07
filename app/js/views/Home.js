d8bba25d12d1363e0d43a254e8ae6700d4a465ef
Olle Werme <olle.werme@gmail.com>
2016-02-07 13:28:07 +0100

    Add section images

--- app/js/views/Home.js
+++ app/js/views/Home.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router'
 const Home = ({
   sections
 }) =>
-  <div style={{padding: '0 1rem', textAlign: 'center'}}>
-    <h1>Welcome to Sweden</h1>
+  <div style={{padding: '1rem', textAlign: 'center', background: 'white', minHeight: '100vh'}}>
+    <h1 style={{margin: '0 0 1rem'}}>Welcome to Sweden</h1>
 
     <ul style={{
       display: 'flex',
@@ -22,18 +22,21 @@ const Home = ({
 
   </div>
 
-const Circle = ({ radius }) =>
+const Image = ({ radius, url }) =>
   <div style={{
     width: radius,
     height: radius,
-    border: '3px solid #eee',
     margin: '0 auto 0.6rem',
-    borderRadius: '100%'
+    borderRadius: '100%',
+    background: `url(${url})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center center',
+    backgroundColor: '#eee'
   }} />
 
-const SectionLink = ({ id, title }) =>
+const SectionLink = ({ id, title, image }) =>
   <Link to={String(id)} style={{textDecoration: 'none'}}>
-    <Circle radius="7rem" />
+    <Image radius="7rem" url={image.url} />
     <div style={{color: '#333', fontSize: '0.9rem'}}>
       {title}
     </div>
